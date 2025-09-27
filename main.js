9bd0e9c1e7045414495dea56b4b975fb2321dfdd
Pastor <test.pd606@gmail.com>
2025-09-28 01:12:10 +0800

    fix: 調整 animation 速度

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
-
 import { initSwipers } from "./assets/js/swiper";
-initSwipers();
-
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-
-AOS.init({
-    duration: 400,
-    easing: 'ease-in-sine',
-});
-
 import { initStickyHeader } from './assets/js/animations';
-initStickyHeader('#nav');
+
+document.addEventListener('DOMContentLoaded', () => {
+  initSwipers();
+
+  AOS.init({
+    duration: 2000,
+    easing: 'ease',
+    once: false,
+  });
+
+  initStickyHeader('#nav');
+});
